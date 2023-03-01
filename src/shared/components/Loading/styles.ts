3aa9e04ee9d48552da4240e3b49c36c624d050e5
lucas santos <lucassantosdasilva1@hotmail.com>
2023-03-01 20:55:01 -0300

    :construciont: fix borderRadius percentage

--- src/shared/components/Loading/styles.ts
+++ src/shared/components/Loading/styles.ts
@@ -4,18 +4,18 @@ export const LoadingContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: #fff;
+  background-color: #1A1A2F;
 `;
 
 export const LoadingText = styled.Text`
   margin-top: 10px;
   font-size: 18px;
   font-weight: bold;
-  color: #333;
+  color: #FFF;
 `;
 
 export const LoadingCircle = styled.View`
-  border-radius: 50%;
+  border-radius: 50;
   width: 40px;
   height: 40px;
   border: 5px solid rgba(0, 0, 0, 0.1);
@@ -29,7 +29,7 @@ export const LoadingCircleFill = styled.View`
   top: -5px;
   right: -5px;
   bottom: -5px;
-  border-radius: 50%;
+  border-radius: 50;
   border: 5px solid transparent;
   border-top-color: #3498db;
   animation: loading-spin 0.5s linear infinite;
